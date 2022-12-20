ef80265ada86be5aeaf05c36f3caaf57a5b80f4a
unknown <home_dv@mail.ru>
2022-12-20 19:18:28 +0300

    fix form

--- js/animations.js
+++ js/animations.js
@@ -17,3 +17,24 @@ function initIncreaseNumberAnimation() {
 }
 
 initIncreaseNumberAnimation();
+
+document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+    if(event.target.value === 'other') {
+        let formContainer = document.createElement('div');
+        formContainer.classList.add('form__group', 'form__other-input');
+
+        let input = document.createElement('input');
+        input.placeholder = 'Введите ваш вариант';
+        input.type = 'text';
+
+        formContainer.appendChild(input);
+
+        document.querySelector('.form form').insertBefore(formContainer, document.querySelector('.form__submit'));
+    }
+
+    let otherInput = document.querySelector('.form__other-input');
+
+    if(event.target.value !== 'other' && Boolean(otherInput)) {
+        document.querySelector('.form form').removeChild(otherInput);
+    }
+});
